997acdaffc899926c7a946c177673429884cc68b
Yasmin <61717234+YasminLuzia@users.noreply.github.com>
2024-01-28 15:56:25 +0000

    Added CORS to communicate with frontend

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8000
 const routeBook = require('./routes/book')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors({origin: '*'}))
 
 app.use('/livros', routeBook)
 
